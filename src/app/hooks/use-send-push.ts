44f096c9f1778f7abbe833517ad9057c8df11d8d
sorlros <sorlros2047@gmail.com>
2024-04-10 16:15:24 +0900

    push기능 호출 및 생성

---
+++ src/app/hooks/use-send-push.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+//수정할것
+const useSendPush = () => {
+  const sendPush = async ({
+    title,
+    body,
+    click_action,
+  }: {
+    title: string;
+    body: string;
+    click_action: string;
+  }) => {
+    const message = {
+      data: {
+        title,
+        body,
+        image: "/logos/favicon-196x196.png",
+        click_action,
+      },
+    };
+
+    axios.request({
+      method: "POST",
+      url: window?.location?.origin + "api/send-fcm",
+      data: { message },
+    });
+  };
+
+  return sendPush;
+};
+
+export default useSendPush;
